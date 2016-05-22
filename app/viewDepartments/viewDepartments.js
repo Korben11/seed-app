e81b603a4a99be83079c53de216d308789e679b9
Juraj Korbacka <jkorbacka@gmail.com>
2016-05-23 01:58:45 +0200

    departments changed to array

--- app/viewDepartments/viewDepartments.js
+++ app/viewDepartments/viewDepartments.js
@@ -9,10 +9,22 @@ angular.module('myApp.viewDepartments',['ngRoute'])
 	 });
 }])
 	 .controller('ViewDepartmentsCtrl', ['$scope', function($scope){
-          $scope.departments = [
-          						{ "department":"learn angular", "done":true },
-								{ "department":"build an angular app", "done":false},
-								{ "department":"something", "done":false },
-								{ "department":"another todo", "done":true }
-								];                
+          $scope.departments = companyDepartments;                
         }]);
+
+	 var companyDepartments = [{
+	 	name: 'IT',
+	 	description: "Information technologies (IT) department is resposible for functionality of all software and eletronics in company",
+	 	people: 13,
+	 	fields: ["front-end", "back-end", "security spacialist"],
+	 	avarageSalary: 1500,
+	 	contact: "it@department.hh"
+	 }, {
+	 	name: 'HR',
+	 	description: "Human resources (HR) department is responsible for all employees in our company as well for interships, partime jobs...",
+	 	people: 5,
+	 	fields: ["recruitment", "monitoring"],
+	 	avarageSalary: 1000,
+	 	contact: "it@department.hh"
+	 }
+	 ];

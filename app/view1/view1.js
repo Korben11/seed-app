2a29e7761f825acd95f8c511af42a7962f9799b2
Juraj Korbacka <jkorbacka@gmail.com>
2016-05-24 15:43:30 +0200

    made lins view1 working

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,4 +11,18 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [function() {
 
-}]);
+}])
+.controller('namesCtrl', function($scope){
+	$scope.tasks = [
+        {eID:'1',task:'Cleanning', time:'17h-18h'},
+        {eID:'2',task:'Cleanning', time:'17h-18h'},
+        {eID:'3',task:'Office', time:'8h-16h'},
+        {eID:'4',task:'Office', time:'8h-16h'},
+        {eID:'5',task:'Managing', time:'8h-16h'},
+        {eID:'6',task:'Managing', time:'8h-16h'},
+        {eID:'7',task:'Scheduling', time:'12h'},
+        {eID:'8',task:'scheduling', time:'12h'},
+        {eID:'9',task:'Watching', time:'8h-16h'},
+        {eID:'10',task:'watching', time:'8h-16h'},
+    ];
+});

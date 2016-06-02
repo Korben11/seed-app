aad27f73cd50d345227fe8c63d6b2a4236bb9d7c
IugaAndreea <iuga_andreea_96@yahoo.com>
2016-06-02 16:04:14 +0200

    changes to employees.cs

--- app/viewEmployees/viewEmployees.js
+++ app/viewEmployees/viewEmployees.js
@@ -10,30 +10,38 @@ angular.module('myApp.viewEmployees', ['ngRoute'])
 }])
 
 .controller("Web2ListEmployees", ['$scope', function($scope){
-          $scope.employees = companyEmployees;                
+          $scope.employees = companyEmployees;   
+          $scope.add = function(x){$scope.tasks.push(x);}
+          $scope.del = function(x){var index = $scope.tasks.indexOf(x);
+                                   $scope.tasks.splice(index, 1);};
+          $scope.edit = function(x){
+          $scope.current = x;};
+          $scope.save = function(x){
+          $scope.current = {};};             
         }]);
 
 
         var companyEmployees = [{
             name: "John Smith",
             id: 123456,
-            age: 31,
-            children: ["Anna", "Mary", "Jullian"],
-            phone: 0678945612,
+            // age: 31,
+            // children: ["Anna", "Mary", "Jullian"],
+            // phone: 0678945612,
             email: "jSmith@netherlands.nl"
        }, {
             name: "John Todd",
             id: 124789,
-            age: 42,
-            children: ["David", "Luan", "Todd"],
-            phone: 0678789612,
+            // age: 42,
+            // children: ["David", "Luan", "Todd"],
+            // phone: 0678789612,
             email: "jTodd@netherlands.nl"
        }, {
             name: "John Michell",
             id: 789289,
-            age: 29,
-            children: ["David"],
-            phone: 0678989612,
+            // age: 29,
+            // children: ["David"],
+            // phone: 0678989612,
             email: "jMichell@netherlands.nl"
        }
        ];
+

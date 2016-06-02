b5643fd4b54822046529d3035708cfb994193fca
Linh <l.hoanglinh@fontys.nl>
2016-06-02 12:49:35 +0200

    nothing to see here

--- app/viewTask/viewTask.js
+++ app/viewTask/viewTask.js
@@ -9,13 +9,6 @@ angular.module('myApp.viewTask', ['ngRoute'])
   });
 }])
 
-.controller('ViewTaskCtrl', [function() {
-
-}])
-.controller('namesCtrl', ['$scope', function($scope){
-	$scope.tasks = companyTask;
-}]);
-
 var companyTask = [
         {eID:'1',task:'Cleanning', time:'17h-18h'},
         {eID:'2',task:'Cleanning', time:'17h-18h'},
@@ -27,4 +20,28 @@ var companyTask = [
         {eID:'8',task:'scheduling', time:'12h'},
         {eID:'9',task:'Watching', time:'8h-16h'},
         {eID:'10',task:'watching', time:'8h-16h'},
-    ]
+    ];
+
+
+.controller('ViewTaskCtrl', [function() {
+
+}])
+.controller('namesCtrl', ['$scope', function($scope){
+$scope.tasks = companyTask;
+
+$scope.del = function(eID){
+var index = getSelectedIndex(eID);
+alert(index);
+$scope.companyTask.splice(index, 1);
+};
+
+function getSelectedIndex(eID){
+    for(var i=0; i<$scope.companyTask.length; i++)    
+        if($scope.companyTask[i].eID == eID)
+            return i;
+        return -1;              
+               
+}
+}]);
+
+
